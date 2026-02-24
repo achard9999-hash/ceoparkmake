02cd454cd51b9e2e85c885fdd3a01bc7ecd3a8d4
achard9999-hash <achard9999@gmail.com>
2026-02-24 11:52:45 +0900

    Create __init__.py

---
+++ game/__init__.py
@@ -0,0 +1,10 @@
+# ceoparkmake/game/__init__.py
+
+from .balance import (
+    RANKS,
+    PROMOTION_FAIL_LIMIT,
+    DEFAULT_FAIL_LIMIT,
+    BASE_PROMOTION_RATE_BY_RANK,
+    WORK_REWARD_BY_RANK,
+    EXP_REQUIREMENT_BY_RANK,
+)
